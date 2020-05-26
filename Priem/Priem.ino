83d43c1e7c33d8931eb27893b7c2b66299155a60
0softic0 <53927748+0softic0@users.noreply.github.com>
2020-05-26 14:34:14 +0300

    Добавлены тесты датчика температуры

--- Priem/Priem.ino
+++ Priem/Priem.ino
@@ -61,6 +61,7 @@ RF24   radio(pinNRFce , pinNRFcsn);	//	Создаём объект radio для 
 int   sendData[2];		//	Создаём массив для приёма данных
 											//	0 - левая гусеница,
 											//	1 - праввая гусеница;
+unsigned long outData;
 constexpr auto leftCat = 0;
 constexpr auto rightCat = 1;
 
@@ -108,13 +109,13 @@ void setup() {
   radio.setChannel(120);                        // Указываем канал приёма данных (от 0 до 127), 5 - значит приём данных осуществляется на частоте 2,405 ГГц (на одном канале может быть только 1 приёмник и до 6 передатчиков)
   radio.setDataRate(RF24_250KBPS);           // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
   radio.setPALevel(RF24_PA_MAX);         // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
-//	radio.enableAckPayload();                                  // Указываем что в пакетах подтверждения приёма есть блок с пользовательскими данными.
+	radio.enableAckPayload();                                  // Указываем что в пакетах подтверждения приёма есть блок с пользовательскими данными.
 	radio.openReadingPipe(1, 0x1234567890LL);    // Открываем 1 трубу с идентификатором 0x1234567890 для приема данных (на ожном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)
 //    radio.openReadingPipe (1, 0x1234567890LL);
   radio.startListening();                     // Включаем приемник, начинаем прослушивать открытую трубу
 //  dataVoltage=analogRead(pinVolt);	//	считали значение напряжения 490 единиц - это 50,2 вольта
-//	radio.writeAckPayload(1, &dataVoltage , sizeof(dataVoltage));
-	delay(100);
+	radio.writeAckPayload(1, &outData , sizeof(outData));
+	delay(200);
   CaterpL.Inic();
   CaterpR.Inic();
 
@@ -126,7 +127,8 @@ void loop()
   if (radio.available()) {                                // Если в буфере имеются принятые данные
 //    printf("Ect data SizeOF=%d \n",sizeof(sendData) );
     radio.read(&sendData, sizeof(sendData));            // Читаем данные в массив data и указываем сколько байт читать
-//		radio.writeAckPayload(1, &dataVoltage, sizeof(dataVoltage));
+		outData=millis();
+		radio.writeAckPayload(1, &dataVoltage, sizeof(dataVoltage));
 //    printf("Lev = %d \t Prav=%d \n", sendData[0],sendData[1]);
     lastDataTime = millis();            //  запомнили время последнего удачного приема
     digitalWrite(startDrivers, HIGH); //  включаем драйвера
---
+++ TestTemperature/TestTemperature.ino
@@ -0,0 +1,34 @@
+﻿#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// линия данных подключена к цифровому выводу 2 Arduino
+#define ONE_WIRE_BUS 2
+
+// настройка объекта oneWire для связи с любым устройством OneWire
+OneWire oneWire(ONE_WIRE_BUS);
+
+// передать ссылку на oneWire библиотеке DallasTemperature
+DallasTemperature sensors(&oneWire);
+
+void setup(void) {
+  sensors.begin();	// запустить библиотеку
+  Serial.begin(9600);
+}
+
+void loop(void) {
+  // отправить команду для получения температуры
+  sensors.requestTemperatures();
+
+  // напечатать температуру в градусах Цельсия
+  Serial.print("Temperature: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print((char)176);// напечатать символ градусов
+  Serial.print("C  |  ");
+
+  // напечатать температуру в градусах Фаренгейта
+  Serial.print((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
+  Serial.print((char)176);// напечатать символ градусов
+  Serial.println("F");
+
+  delay(500);
+}
